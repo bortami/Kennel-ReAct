d1d3ddfe5b424f0e2a98e926b16a27581d134795
Michelle Tabor <mtabor66@outlook.com>
2019-03-06 11:40:26 -0500

    added fetch calls

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,44 +3,39 @@ import React, { Component } from 'react';
 import AnimalList from './animals/AnimalList';
 import LocationList from './locations/LocationsList';
 import EmployeeList from './employee/EmployeeList';
-import OwnersList from "./owners/OwnersList"
+import OwnersList from './owners/OwnersList';
 
 class ApplicationViews extends Component {
-	employeesFromAPI = [
-		{ id: 1, name: 'Jessica Younker' },
-		{ id: 2, name: 'Jordan Nelson' },
-		{ id: 3, name: 'Zoe LeBlanc' },
-		{ id: 4, name: 'Blaise Roberts' }
-	];
-
-	locationsFromAPI = [
-		{ id: 1, name: 'Nashville North', address: '500 Circle Way' },
-		{ id: 2, name: 'Nashville South', address: '10101 Binary Court' }
-	];
-
-	animalsFromAPI = [
-		{ id: 1, name: 'Doodles', ownerId: 1 },
-		{ id: 2, name: 'Jack', ownerId: 2 },
-		{ id: 3, name: 'Angus', ownerId: 3 },
-		{ id: 4, name: 'Henley', ownerId: 4 },
-		{ id: 5, name: 'Derkins', ownerId: 5 },
-		{ id: 6, name: 'Checkers', ownerId: 6 }
-	];
-	ownersFromAPI = [
-		{ id: 1, name: 'Ryan Tanay', phoneNumber: '304-555-2525' },
-		{ id: 2, name: 'Emma Beaton', phoneNumber: '304-555-2525' },
-		{ id: 3, name: 'Dani Adkins', phoneNumber: '304-555-2525' },
-		{ id: 4, name: 'Adam Oswalt', phoneNumber: '304-555-2525' },
-		{ id: 5, name: 'Fletcher Bangs', phoneNumber: '304-555-2525' },
-		{ id: 6, name: 'Angela Lee', phoneNumber: '304-555-2525' }
-	];
-
 	state = {
-		employees: this.employeesFromAPI,
-		locations: this.locationsFromAPI,
-		animals: this.animalsFromAPI,
-		owners: this.ownersFromAPI
+		employees: [],
+		locations: [],
+		animals: [],
+		owners: []
 	};
+	componentDidMount() {
+		const newState = {};
+		fetch('http://localhost:5002/employees')
+			.then((employees) => employees.json())
+			.then((parsedEmployees) => {
+				newState.employees = parsedEmployees;
+				return fetch('http://localhost:5002/locations');
+			})
+			.then((locations) => locations.json())
+			.then((parsedLocations) => {
+				newState.locations = parsedLocations;
+				return fetch('http://localhost:5002/owners');
+			})
+			.then((r) => r.json())
+			.then((parsedOwners) => {
+				newState.owners = parsedOwners;
+				return fetch('http://localhost:5002/animals');
+			})
+			.then((r) => r.json())
+			.then((parsedAnimals) => {
+				newState.animals = parsedAnimals;
+			})
+			.then(() => this.setState(newState));
+	}
 
 	render() {
 		return (
