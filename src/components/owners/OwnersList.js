fe8473b549af146ee46d436df024cabee1dd00bd
Michelle Tabor <mtabor66@outlook.com>
2019-03-07 15:49:05 -0500

    added gold membership

--- src/components/owners/OwnersList.js
+++ src/components/owners/OwnersList.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import '../animals/Animal.css';
 
 export default class OwnerList extends Component {
+	isGold=(person)=>{
+		if(person.goldMember){
+			return "gold"
+		}else{}
+	}
 	render() {
 		return (
 			<article clasName="owners">
 				{this.props.owners.map((singleowner) => (
 					<div key={singleowner.id} className="card">
-						<div className="card-body">
+						<div className="card-body" id={this.isGold(singleowner)}>
 							<h5 className="card-title">
 								{singleowner.name}</h5>
 								<p>{singleowner.phoneNumber}</p>
