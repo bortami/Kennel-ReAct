49db3b7d5df37911e405b1dad5eaec872aeca585
Michelle Tabor <mtabor66@outlook.com>
2019-03-07 15:32:28 -0500

    added animal icons logic

--- src/components/animals/AnimalList.js
+++ src/components/animals/AnimalList.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import dog from './DogIcon.png';
+import cat from "./cat-icon.png"
+import bird from "./bird-icon.png"
+import reptile from "./reptile-icon.png"
+import rodent from "./rodent-icon.png"
+import cow from "./Cow-Icon.png"
 import './Animal.css';
 
 export default class AnimalList extends Component {
+	picturepicker=(speciesParam)=>{
+		if(speciesParam === "dog"){return dog}
+		else if(speciesParam === "cat"){return cat}
+		else if(speciesParam === "bird"){return bird}
+		else if(speciesParam ==="reptile"){return reptile}
+		else if(speciesParam ==="rodent"){return rodent}
+		else{return cow}
+	}
+	;
 	render() {
 		const animalsWithOwners = this.props.animals.map((singleAnimal) => {
 			const matchingAnimalOwnerEntries = this.props.animalOwners.filter((singleAnimalOwnerEntry) => {
@@ -29,7 +43,7 @@ export default class AnimalList extends Component {
 					<div key={animal.id} className="card">
 						<div className="card-body">
 							<h5 className="card-title">
-								<img src={dog} className="icon--dog" alt="cute dog icon" />
+								<img src={this.picturepicker(animal.species)} className="icon--dog" alt="cute dog icon" />
 								{animal.name}
 							</h5>{' '}
 							<p className="text-center">{animal.species}</p>
